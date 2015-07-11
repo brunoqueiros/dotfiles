3c69a3107fb913a658a16eb5e06650979caf34f5
Bruno Queiros <brunoofq@gmail.com>
2015-07-11 17:13:27 -0300

    Create script to install Eclipse

---
+++ applications/eclipse/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./bootstrap.sh
+
+NAME="Eclipse Mars EE"
+URL="http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/luna/SR2/eclipse-java-luna-SR2-linux-gtk-x86_64.tar.gz"
+FILE="eclipse.tar.gz"
+
+info "Installing $NAME"
+
+download_source "$URL" "$FILE"
+extract_tar "$FILE" "/opt/teste/"
+sudo sh -c 'echo "[Desktop Entry]
+Name=Eclipse 4
+Type=Application
+Exec=/opt/eclipse/eclipse
+Terminal=false
+Icon=/opt/eclipse/icon.xpm
+Comment=Integrated Development Environment
+NoDisplay=false
+Categories=Development;IDE;
+Name[en]=Eclipse" >> "/usr/share/applications/eclipse.desktop"'
+rm "$FILE"
+
+success "$NAME installed"
