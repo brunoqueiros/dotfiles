c49fe637b81b849e7d0eaa24eb5707c2d1bc65a0
Bruno Queiros <brunoofq@gmail.com>
2015-07-11 17:36:30 -0300

    Create script to install Opera

---
+++ applications/opera/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./bootstrap.sh
+
+NAME="Opera"
+URL="http://get.geo.opera.com.global.prod.fastly.net/pub/opera/desktop/30.0.1835.88/linux/opera-stable_30.0.1835.88_amd64.deb"
+FILE="opera.deb"
+
+info "Installing $NAME"
+
+if test ! "$(which opera)"; then
+  download_source "$URL" "$FILE"
+  install_dpkg "$FILE"
+  rm "$FILE"
+fi
+
+success "$NAME installed"
