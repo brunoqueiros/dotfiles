37e78756916d23be6f5aa58c6225e704aeb1fcfa
Bruno Queiros <brunoofq@gmail.com>
2015-07-12 23:59:02 -0300

    Create .tmp folder on install

--- install.sh
+++ install.sh
@@ -4,12 +4,24 @@ source bootstrap.sh
 
 is_linux
 
-main () {
+create_tmp_folder() {
+  mkdir .tmp
+}
+
+remove_tmp_folder() {
+  rm -R .tmp
+}
+
+main() {
+  create_tmp_folder
+
   info "Installing dependencies"
   check_dependencies
   success "Dependencies installed"
 
   find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
+
+  remove_tmp_folder
 }
 
 main
