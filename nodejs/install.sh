563615839109a2083c58d405862adeedcecabe97
Bruno Queiros <brunoofq@gmail.com>
2015-09-29 10:16:29 -0300

    Update to nodejs 4

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -7,7 +7,7 @@ NAME="Nodejs"
 info "Installing $NAME"
 
 if test ! "$(which node)"; then
-  curl -sL https://deb.nodesource.com/setup | sudo bash -
+  curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
   install "nodejs"
   install "build-essential"
 fi
